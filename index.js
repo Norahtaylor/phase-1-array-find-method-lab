c89a830f693b16760696d12c241a2e77e8dd3edc
[Norah Taylor] <emt368@gmail.com>
2023-02-01 13:51:37 -0500

    commit tree 3c6599410e2204f457a433c4c047039fb7247c7f

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// code your solution here
+function superbowlWin(array){
+    if(array.result === "W"){
+        return array.year
+    } else return undefined
+}
+
+
+
+// function superbowlWin(array){
+//     const win = array.find(win => win.result === "W");
+//         if (win) {
+//             return win.year;
+//         }
+//             else {
+//                 return win; }
+//     }
+
+    //need to use arrow function?!!!
+
+// let superbowlWin = record.find(result => {
+//     if (result === "W"){
+//         return result.year;
+//     }
+//             else {
+//                 return "undefined"
+//             }
+//         });
+// function superbowlWin(record){
+//     let result;
+//     if(result === "W"){
+//         return result.year; }
+//         else {
+//             return "undefined"
+//         }
+//         // return result 
+//     }
+// superbowlWin.record.find()
+
+
+    //for (let item of array)
+//     const found = record.find(result === "w" )
+       
+//        // if (record.find(result === "W"); {
+//             return 'record[year]'
+//         }
+            
+//             }
+//                 else return 'undefined'
+//             }}
+//             //return result;
+        
+//        // superbowlWin(record)
+
+//     //record.find("W")
+
+// //     if (array === w)
+// //     return record.year
+// // 
+
+// function superbowlWin(array){
+//     return (result === "W");
+// }
+
+// record.find(superbowlWin)
